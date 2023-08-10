3a2064983fef361688c414ed9d19623b6f269286
satriafy2 <msatriadwi@gmail.com>
2023-08-10 17:40:46 +0700

    unit-test: initial

--- src/main/java/com/satriadwi/employee/dto/EmployeeDto.java
+++ src/main/java/com/satriadwi/employee/dto/EmployeeDto.java
@@ -5,9 +5,13 @@ import javax.validation.constraints.Size;
 
 import org.hibernate.validator.constraints.Range;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class EmployeeDto {
 
     @NotEmpty
